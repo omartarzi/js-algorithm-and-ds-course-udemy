07d7023e9745460fa869ae26ae159a4a3fd03c4b
Omar El Tarzi <omartarzi@gmail.com>
2020-12-10 14:22:40 +0200

    Completed anagrams with and without course.

---
+++ anagrams.js
@@ -0,0 +1,65 @@
+// Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+// validAnagram('', '') // true
+// validAnagram('aaz', 'zza') // false
+// validAnagram('anagram', 'nagaram') // true
+// validAnagram("rat","car") // false) // false
+// validAnagram('awesome', 'awesom') // false
+// validAnagram('qwerty', 'qeywrt') // true
+// validAnagram('texttwisttime', 'timetwisttext') // true
+
+function validAnagram(str1, str2) {
+  // add whatever parameters you deem necessary - good luck!
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let char of str1) {
+    frequencyCounter1[char] = (frequencyCounter1[char] || 0) + 1;
+  }
+  for (let char of str2) {
+    frequencyCounter2[char] = (frequencyCounter2[char] || 0) + 1;
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key] !== frequencyCounter1[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//FROM COURSE
+
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    // if letter exists, increment, otherwise set to 1
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+  }
+  console.log(lookup)
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+
+  return true;
+}
+
+// {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
+validAnagram('anagrams', 'nagaramm')
