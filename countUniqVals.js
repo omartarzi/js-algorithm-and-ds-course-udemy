2b42af284b905146b72f98de5e1474aacd5e37b7
Omar El Tarzi <omartarzi@gmail.com>
2020-12-10 16:57:19 +0200

    Completed countUniqueValues with and without course.

---
+++ countUniqVals.js
@@ -0,0 +1,29 @@
+// Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
+
+// countUniqueValues([1,1,1,1,1,2]) // 2
+// countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]) // 7
+// countUniqueValues([]) // 0
+// countUniqueValues([-2,-1,-1,0,1]) // 4
+
+function countUniqueValues(arr){
+  // add whatever parameters you deem necessary - good luck!
+  let result = {};
+  for (let val of arr) {
+      result[val] ? result[val] += 1 : result[val] = 1 ;
+  }
+  return Object.keys(result).length;
+}
+
+//refactored
+
+function countUniqueValues(arr){
+    if(arr.length === 0) return 0;
+    var i = 0;
+    for(var j = 1; j < arr.length; j++){
+        if(arr[i] !== arr[j]){
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1;
+}
