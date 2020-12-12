371869c7e8fdc6f99d0212408fda6487e1135820
Omar El Tarzi <omartarzi@gmail.com>
2020-12-12 13:53:40 +0200

    Completed quick sort.

---
+++ quickSort.js
@@ -0,0 +1,86 @@
+// Like merge sort, exploits the fact that arrays of 0 or 1 element are always sorted
+// Works by selecting one element (called the "pivot") and finding the index where the pivot should end up in the sorted array
+// Once the pivot is positioned appropriately, quick sort can be applied on either side of the pivot
+
+// In order to implement merge sort, it's useful to first implement a function responsible arranging elements in an array on either side of a pivot
+// Given an array, this helper function should designate an element as the pivot
+// It should then rearrange elements in the array so that all values less than the pivot are moved to the left of the pivot, and all values greater than the pivot are moved to the right of the pivot
+// The order of elements on either side of the pivot doesn't matter!
+// The helper should do this in place, that is, it should not create a new array
+// When complete, the helper should return the index of the pivot
+
+// Worst case is if the array is already sorted (if the pivot is always the smallest - or largest - element)
+
+
+// First Version
+function pivot(arr, start=0, end=arr.length+1){
+  function swap(array, i, j) {
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  var pivot = arr[start];
+  var swapIdx = start;
+
+  for(var i = start + 1; i < arr.length; i++){
+    if(pivot > arr[i]){
+      swapIdx++;
+      swap(arr,swapIdx,i);
+    }
+  }
+  swap(arr,start,swapIdx);
+  return swapIdx;
+}
+
+// Version with ES2015 Syntax
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  // We are assuming the pivot is always the first element
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+
+  // Swap the pivot from the start the swapPoint
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+pivot([4,8,2,1,5,7,6,3])
+
+
+
+
+function quickSort(arr, left = 0, right = arr.length -1){
+    if(left < right){
+        let pivotIndex = pivot(arr, left, right) //3
+        //left
+        quickSort(arr,left,pivotIndex-1);
+        //right
+        quickSort(arr,pivotIndex+1,right);
+      }
+     return arr;
+}
+
+quickSort([100,-3,2,4,6,9,1,2,5,3,23])
+
+
+
+
+// [4,6,9,1,2,5,3]
+// [3,2,1,4,6,9,5]
+//        4
+//  3,2,1    6,9,5
+//      3      6
+//  2,1      5  9
+//    2
+//  1
